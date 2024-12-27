8fbd04323d6e646500093505ddbfeb0662ff9928
RamyRxr <reyr06115@gmail.com>
2024-12-27 11:43:45 -0500

    just few update to see the result of other file code

--- Client/app/dashboard/page.tsx
+++ Client/app/dashboard/page.tsx
@@ -16,13 +16,13 @@ const Dashboard = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
             
-            <CardPopularProducts />
+            {/* <CardPopularProducts />
 
             <CardSalesSummary />
 
             <CardPurchaseSummary />
 
-            <CardExpenseSummary />
+            <CardExpenseSummary /> */}
             
             <StatCard
                 title="Customer & Expenses"
